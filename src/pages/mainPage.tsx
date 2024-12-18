86c70227c2de491f0a8f7d4357014f75286ea9ac
pauleenaphan <pauleena2002@gmail.com>
2024-12-17 21:45:37 -0800

    remodeled mainpage

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,19 +1,24 @@
 import "../styles/mainPage.css";
-import moonImg from "../assets/moon.png";
-import mouse from "../assets/mouse.png";
-import rocket from "../assets/rocket.png";
-import { BsMoonStars } from "react-icons/bs";
+import moonImg from "../assets/mainPage/moon.png";
+import mouse from "../assets/mainPage/mouse.png";
+import rocket from "../assets/mainPage/rocket.png";
+import logo from "../assets/mainpage/logo.png";
+
+import { RiPagesFill } from "react-icons/ri";
+import { IoCheckmark } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa";
 
 export const MainPage = () =>{
     return(
         <div className="mainPage">
             <nav> 
-                <div className="logoIconContainer">
-                    <BsMoonStars id="moonIcon"/>
-                    <div id="logo"> FirstLanding </div>
+                <img id="logo" src={logo} alt="logo"/>
+                
+                <div className="exploreContainer">
+                    <RiPagesFill />
+                    <p> Explore </p>
                 </div>
                 
-                <button> Explore Pages </button>
             </nav>
 
             <section className="aboutContainer">
@@ -35,9 +40,9 @@ export const MainPage = () =>{
                 <h2> 
                     "First Landing gave me the experience of looking at these creative sterotypically landing pages 
                     that all follow the same format. I would reccomend this experience to all my friends and everyone
-                    I know. P.S. I was paid to write this review."
+                    I know."
                 </h2>
-                <p> - Your Mom </p>
+                <p> - Vector from minions</p>
             </section>
 
             <div className="blockContainer">
@@ -68,50 +73,83 @@ export const MainPage = () =>{
             <section className="planContainer">
                 <h2> Choose the plan that works for you </h2>
                 <div className="planInnerContainer">
-                    <div className="priceContainer">
-                        <h3> Free </h3>
-                        <p> We all love free </p>
-                        <div className="planPrice"> $0 </div>
-                        <button className="priceBtns"> Get Free </button>
-                        <ul className="planBenefits">
+                <div className="priceContainer">
+                    <h3> Free </h3>
+                    <p> We all love free </p>
+                    <div className="planPrice"> $0 </div>
+                    <button className="priceBtns"> Get Free </button>
+                    <ul className="planBenefits">
+                        <div className="benefitContainer">
+                            <IoCheckmark className="checkMarkIcon"/>
                             <li> All views included </li>
+                        </div>
+                        <div className="benefitContainer">
+                            <IoCheckmark className="checkMarkIcon"/>
                             <li> No Charges Necessary </li>
+                        </div>
+                        <div className="benefitContainer">
+                            <IoCheckmark className="checkMarkIcon"/>
                             <li> Unlimited Views </li>
-                        </ul>
-                    </div>
-                
-
-                    <div className="priceContainer">
-                        <h3> Basic </h3>
-                        <p> View with some perks </p>
-                        <div className="planPrice"> $5 </div>
-                        <button className="priceBtns"> Get Basic </button>
-                        <ul className="planBenefits">
-                            <li> Everything in Free </li>
-                            <li> 1 Week early view access </li>
-                            <li> Free t-shirt </li>
-                        </ul>
-                    </div>
-
-                    <div className="priceContainer">
-                        <h3> Premium </h3>
-                        <p> Best viewing experience </p>
-                        <div className="planPrice"> $500 </div>
-                        <button className="priceBtns"> Get premium </button>
-                        <ul className="planBenefits">
-                            <li> Everything in basic  </li>
-                            <li> 24 Hour early view access </li>
-                            <li> Free t-shirt, hat, and keychain </li>
-                        </ul>
-                    </div>
+                        </div>
+                    </ul>
                 </div>
-                <p id="captionMsg"> Less saving and start viewing </p>
+
+                <div className="priceContainer">
+                    <h3> Basic </h3>
+                    <p> View with some perks </p>
+                    <div className="planPrice"> $5 </div>
+                    <button className="priceBtns"> Get Basic </button>
+                    <ul className="planBenefits">
+                        <div className="benefitContainer">
+                            <IoCheckmark className="checkMarkIcon"/>
+                            <li> Everything in Free </li>
+                        </div>
+                        <div className="benefitContainer">
+                            <IoCheckmark className="checkMarkIcon"/>
+                            <li> 1 Week early view access </li>
+                        </div>
+                        <div className="benefitContainer">
+                            <IoCheckmark className="checkMarkIcon"/>
+                            <li> Free t-shirt </li>
+                        </div>
+                    </ul>
+                </div>
+
+                <div className="priceContainer">
+                    <h3> Premium </h3>
+                    <p> Best viewing experience </p>
+                    <div className="planPrice"> $500 </div>
+                    <button className="priceBtns"> Get Premium </button>
+                    <ul className="planBenefits">
+                        <div className="benefitContainer">
+                            <IoCheckmark className="checkMarkIcon"/>
+                            <li> Everything in Basic </li>
+                        </div>
+                        <div className="benefitContainer">
+                            <IoCheckmark className="checkMarkIcon"/>
+                            <li> 24 Hour early view access </li>
+                        </div>
+                        <div className="benefitContainer">
+                            <IoCheckmark className="checkMarkIcon"/>
+                            <li> Free t-shirt, hat, and keychain </li>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+            <p id="captionMsg"> Less saving and start viewing </p>
             </section>
 
             <section className="captionBtnContainer">
-                <h4> Ready to start viewing the first landing? </h4>
+                <h4> Ready to view the first landing? </h4>
                 <button className="getStartBtn"> Get Started - It's free </button>
             </section>
+
+            <footer>
+                <p> Copyright @ Pauleena Phan 2024 </p>
+                <a href="https://github.com/pauleenaphan/First-Landing" target="_blank" rel="noopener noreferrer">
+                    <FaGithub id="githubIcon"/>
+                </a>
+            </footer>
         </div>
     )
 }
