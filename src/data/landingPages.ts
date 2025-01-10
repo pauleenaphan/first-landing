ce122c67b28505d0df9fb9b4167582659879542a
pauleena phan <pauleena2002@gmail.com>
2025-01-10 14:14:02 -0800

    added cozy sprouts info

--- src/data/landingPages.ts
+++ src/data/landingPages.ts
@@ -1,5 +1,6 @@
 import firstLandingCover from "../assets/mainpage/cover.png";
 import beeLiftCover  from "../assets/beeLift/cover.png";
+import cozySproutCover from "../assets/cozySprout/cover.png";
 
 export const landingPagesData = [
     {
@@ -17,5 +18,13 @@ export const landingPagesData = [
         date: "12/30/2024",
         img: beeLiftCover,
         navigate: "/beeLift"
+    },
+    {
+        name: "cozySprouts",
+        description: "CozySprouts is a company that promotes and sells healthy green plants",
+        tools: ["HTML, Tailwind CSS"],
+        date: "1/10/2025",
+        img: cozySproutCover,
+        navigate: "/"
     }
 ];
